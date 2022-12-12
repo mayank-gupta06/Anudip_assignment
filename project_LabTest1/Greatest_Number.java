ab8aa0704b881cd3039ebf39c4118a55be1ced7f
mayank-gupta06 <gmayank683@gmail.com>
2022-12-12 14:35:44 +0530

    Add files via upload

---
+++ project_LabTest1/Greatest_Number.java
@@ -0,0 +1,24 @@
+package project_LabTest1;
+
+public class Greatest_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=1;
+   int b=10;
+   int c=100;
+   greatest(a,b,c);
+   
+	}
+	public static void greatest(int a,int b,int c) {
+		if(a>b && a>c) {
+			System.out.print(a);
+		}
+		else if(b>a && b>c) {
+			System.out.print(b);
+		}
+		else{
+			System.out.print(c);
+		}
+}
+}
---
+++ project_LabTest1/Odd_numbers.java
@@ -0,0 +1,18 @@
+package project_LabTest1;
+
+public class Odd_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int number=20;
+  System.out.print("List of odd numbers from 1 to "+number+":");
+  for(int i=1; i<=number; i++)
+  {
+	if (i%2!=0)
+	{
+		System.out.print(i+" ");		
+	}
+  }
+}
+}
+
